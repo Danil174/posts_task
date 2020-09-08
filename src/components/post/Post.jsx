afc1987ecc0c7c310a46c46b830049aadb6a890d
danil milostiviy <milostiviy.danil@yandex.ru>
2020-09-08 16:50:20 +0300

    filter posts in posts-list component

---
+++ src/components/post/Post.jsx
@@ -0,0 +1,78 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Item = styled.li`
+    position: relative;
+    display: flex;
+    flex-direction: row;
+    margin-top: 10px;
+    padding: 10px;
+    background-color: ${props => props.theme.mainPostColor};
+    border-radius: 13px;
+    box-shadow: 1px 1px 5px -2px #9b93e9;
+
+    .block {
+        display: flex;
+        flex-direction: column;
+        align-items: flex-start;
+    }
+
+    .user_block {
+        flex-shrink: 0;
+        margin-right: 15px;
+    }
+
+  .ico {
+    align-self: center;
+    position: relative;
+    width: 70px;
+    height: 70px;
+    background-color: ${props => props.theme.postGray};
+    border-radius: 50%;
+    overflow: hidden;
+  }
+
+  .ico::after,
+  .ico::before {
+    content: '';
+    position: absolute;
+    left: 50%;
+    transform: translateX(-50%);
+  }
+
+  .ico::after {
+    top: 14px;
+    width: 40px;
+    height: 40px;
+    border-radius: 50%;
+    background-color: ${props => props.theme.postBlue};
+  }
+
+  .ico::before {
+    top: 41px;
+    width: 0;
+    height: 0;
+    border-left: 30px solid transparent;
+    border-right: 30px solid transparent;
+    border-bottom: 40px solid ${props => props.theme.postBlue};
+  }
+`;
+
+const Post = ({post}) => {
+    const {name, username, title, body} = post;
+    return (
+    <Item>
+        <div className="block user_block">
+            <span className="ico"></span>
+            <span><strong>Name: </strong>{name}</span>
+            <span><strong>UserName: </strong>{username}</span>
+        </div>
+        <div className="block text_block">
+            <h3>{title}</h3>
+            <p>{body}</p>
+        </div>
+    </Item>
+    );
+}
+
+export default Post;
---
+++ src/components/posts-list/PostsList.jsx
@@ -0,0 +1,67 @@
+import React, {useState,  useEffect} from 'react';
+import styled from 'styled-components';
+
+import Post from "../post/Post.jsx";
+
+const Input = styled.input`
+    border: none;
+    box-shadow: none;
+    background-color: #f3f5f9;
+    color: #363838;
+    font-size: 14px;
+    padding: 8px 19px;
+    height: 38px;
+
+    :focus {
+        outline: 2px solid #5583b5;
+    }
+`;
+
+const PostsList = ({posts, loading}) => {
+    const [searchStr, setSearch] = useState("");
+    const [filteredPosts, setFilteredPosts] = useState([]);
+
+    useEffect(() => {
+        setFilteredPosts(
+            posts.filter((post) =>
+                post.title.toLowerCase().includes(searchStr.toLowerCase())
+            )
+        );
+        
+        console.log(`filter`);
+    }, [searchStr, posts]);
+
+    if (loading) {
+        return (<strong>Данные загружаются...</strong>)
+    }
+
+    return (
+        <div>
+            <Input
+                type="text"
+                placeholder="filter posts"
+                onChange={(evt) => setSearch(evt.target.value)}
+                value = {searchStr}
+            />
+            <br />
+            {
+                filteredPosts.length === 0 
+                ? 
+                <strong>Соответствий не найдено</strong> 
+                :
+                <ul>
+                {filteredPosts.map((it) => {
+                        return (
+                            <Post
+                                key={it.id}
+                                post={it}
+                            />
+                        )
+                })}
+            </ul>
+            }
+        </div>
+    );
+}
+
+export default PostsList;
